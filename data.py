440e9a99a921252388b5099afafb56cb29a2760e
hanna <hannallory@gmail.com>
2026-07-20 13:30:52 +0200

    Todas as 5 tarefas foram concluidas

--- data.py
+++ data.py
@@ -1,1 +1,7 @@
-#Desculpe, eu enviei sem querer para a revisão no momento em que estava vinculando a minha conta aqui no GitHub.
+URBAN_ROUTES_URL = 'https://cnt-eb0db000-b6fd-42bc-bca0-13ec4d9a5f49.containerhub.tripleten-services.com?lng=pt'
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Pare no bar de sucos'
--- main.py
+++ main.py
@@ -1,2 +1,45 @@
+from data import *
+from helpers import *
 
 class TestUrbanRoutes:
+    @classmethod
+    def setup_class(cls):
+        if is_url_reachable(URBAN_ROUTES_URL):
+            return True
+        else:
+            return False
+
+    def  test_set_route(self):
+         # Adicionar em S8
+         print ("função criada para definir a rota")
+         pass
+    def test_select_plan (self):
+         # Adicionar em S8
+         print ("função criada para definir a plan")
+         pass
+    def test_fill_phone_number (self):
+         # Adicionar em S8
+         print("função criada para definir o telefone")
+         pass
+    def test_fill_card (self):
+         # Adicionar em S8
+         print("função criada para definir o cartao de credito")
+         pass
+    def test_comment_for_driver (self):
+         # Adicionar em S8
+         print("função criada para definir comentario do motorista")
+         pass
+    def test_order_blanket_and_handkerchiefs (self):
+         # Adicionar em S8
+         print("função criada para pedir cobertor")
+         pass
+    def test_order_2_ice_creams (self):
+         # Adicionar em S8
+         print("função criada para pedir 2 sorvetes")
+         for i in range (2):
+             # Adicionar em S8
+             pass
+    def test_car_search_model_appears (self):
+         # Adicionar em S8
+         print("função criada para buscar modelos de carro")
+         pass
